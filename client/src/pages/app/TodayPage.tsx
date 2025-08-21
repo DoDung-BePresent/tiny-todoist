92944a49a3de459e98d5a63ab2146946244df0b0
Đỗ Quang Dũng <111267984+DoDung-BePresent@users.noreply.github.com>
2025-08-21 15:23:09 +0700

    Refactor application structure and implement routing - Updated App component to use RouterProvider for routing. - Removed unused assets including react.svg. - Integrated Tailwind CSS and updated global styles in index.css. - Configured TypeScript paths for easier imports. - Set up Vite configuration to resolve aliases. - Created new layout components: AppLayout and RootLayout. - Added utility function for class name merging. - Implemented basic pages: HomePage, InboxPage, TodayPage, UpcomingPage, LoginPage, and RegisterPage. - Established routing structure with nested routes for authentication and app features.

---
+++ client/src/pages/app/TodayPage.tsx
@@ -0,0 +1,5 @@
+const TodayPage = () => {
+  return <div>TodayPage</div>;
+};
+
+export default TodayPage;
---
+++ client/src/routes/index.tsx
@@ -0,0 +1,70 @@
+/**
+ * Node modules
+ */
+import { createBrowserRouter } from 'react-router';
+
+/**
+ * Pages
+ */
+import LoginPage from '@/pages/auth/LoginPage';
+import RegisterPage from '@/pages/auth/RegisterPage';
+import InboxPage from '@/pages/app/InboxPage';
+import TodayPage from '@/pages/app/TodayPage';
+import UpcomingPage from '@/pages/app/UpcomingPage';
+
+/**
+ * Layouts
+ */
+import AppLayout from '@/layouts/AppLayout';
+import RootLayout from '@/layouts/RootLayout';
+
+/**
+ * Types
+ */
+import type { RouteObject } from 'react-router';
+import HomePage from '@/pages/HomePage';
+
+const rootRoutesChildren: RouteObject[] = [
+  {
+    index: true,
+    element: <HomePage />,
+  },
+  {
+    path: 'register',
+    element: <RegisterPage />,
+  },
+  {
+    path: 'login',
+    element: <LoginPage />,
+  },
+];
+
+const appRoutesChildren: RouteObject[] = [
+  {
+    path: 'inbox',
+    element: <InboxPage />,
+  },
+  {
+    path: 'today',
+    element: <TodayPage />,
+  },
+  {
+    path: 'upcoming',
+    element: <UpcomingPage />,
+  },
+];
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: rootRoutesChildren,
+  },
+  {
+    path: '/app',
+    element: <AppLayout />,
+    children: appRoutesChildren,
+  },
+]);
+
+export default router;
