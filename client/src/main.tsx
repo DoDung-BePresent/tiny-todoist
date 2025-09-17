7cfedefd1fc14f9d12a9e25933052a6056e7ca71
Đỗ Quang Dũng <111267984+DoDung-BePresent@users.noreply.github.com>
2025-09-17 15:02:47 +0700

    feat: replace react-helmet-async with react-helmet and update related dependencies

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,7 +5,6 @@ import App from './App.tsx';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from '@/components/ui/sonner.tsx';
-import { HelmetProvider } from 'react-helmet-async';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,9 +18,7 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
+      <App />
       <Toaster
         richColors
         theme='light'
